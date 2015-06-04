a633d94033fbf8317d4643b3f2d14a8f9fe60f2b
adiekaye <adiekaye@users.noreply.github.com>
2015-06-04 10:29:33 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='adiestats',
+      version='0.1',
+      description='Stats package written for practice',
+      url='http://github.com/adiekaye/adiestats',
+      author='Adie Kaye',
+      author_email='adiekaye@users.noreply.github.com',
+      license='MIT',
+      packages=['adiestats'],
+      zip_safe=False)
