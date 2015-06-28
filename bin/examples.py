c638149b98cb7de6163f8cef68129a53362f362b
adiekaye <adiekaye@users.noreply.github.com>
2015-06-28 17:12:50 +0200

    tidying

---
+++ bin/examples.py
@@ -0,0 +1,14 @@
+""" import math
+import sample_sets
+import ave
+import var """
+
+from ..adiestats import *
+
+a = ave.weighted_mean(sample_sets.paired_set1a, sample_sets.paired_set1b)
+
+print(a)
+
+b = var.sample_cv(sample_sets.set5)
+
+print(b)
