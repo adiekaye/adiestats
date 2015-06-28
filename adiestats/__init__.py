4108adbb3da3bf2c367ac0d1f45af21d6e610b52
adiekaye <adiekaye@users.noreply.github.com>
2015-06-28 22:59:59 +0200

    filled in __init__ with imports to make package useable

---
+++ adiestats/__init__.py
@@ -0,0 +1,5 @@
+from . import ave
+from . import sample_sets
+from . import var
+
+__all__ = ["ave","sample_sets","var"]
--- bin/examples.py
+++ bin/examples.py
@@ -2,18 +2,18 @@ import sys
 import os.path
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from adiestats import ave, sample_sets, var
+import adiestats
 
-a = ave.weighted_mean(sample_sets.paired_set1a, sample_sets.paired_set1b)
+a = adiestats.ave.weighted_mean(adiestats.sample_sets.paired_set1a, adiestats.sample_sets.paired_set1b)
 
 print(a)
 
-b = var.cv(sample_sets.set5)
+b = adiestats.var.cv(adiestats.sample_sets.set5)
 
 print(b)
 
-c = ave.mean_grouped(sample_sets.m,sample_sets.f)
+c = adiestats.ave.mean_grouped(adiestats.sample_sets.m,adiestats.sample_sets.f)
 print(c)
 
-c = var.sd_grouped(sample_sets.m,sample_sets.f)
+c = adiestats.var.sd_grouped(adiestats.sample_sets.m,adiestats.sample_sets.f)
 print(c)
