fe8486bf302ddfc943fe637d5e422a77cb24189f
adiekaye <adiekaye@users.noreply.github.com>
2015-06-29 10:03:41 +0200

    reintroduced weird behaviour to make it run at all - only running example.py ????

--- cov_cor.py
+++ cov_cor.py
@@ -1,9 +1,5 @@
 #examples for using the averages and variability modules
 
-import sys
-import os.path
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '...'))
 import adiestats
 from data import sample_sets as data
 
@@ -20,3 +16,15 @@ print(c)
 
 c = adiestats.var.sd_grouped(data.m,data.f)
 print(c)
+
+#examples for using the relationships module
+
+import sys
+import os.path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import adiestats
+from data import sample_sets as data
+
+a = adiestats.cov(data.x,data.y)
+print(a)
